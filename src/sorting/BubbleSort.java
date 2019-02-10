9a028b9fae90bb35b25782f95a64b83b73cab0b8
harshit <harshit@harshit-HP-Pavilion-15-Notebook-PC>
2019-02-10 21:03:12 +0530

    Applied new sort

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -3,24 +3,24 @@ import java.util.Scanner;
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		int[] a = new int[10];
+		int[] p = new int[10];
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter the list of numbers");
-		for(int i=0;i<a.length;i++) {
-			a[i]= in.nextInt();
+		for(int i=0;i<p.length;i++) {
+			p[i]= in.nextInt();
 		}
 		int temp=0;
-		for(int i=0;i<a.length-1;i++) {
-			for(int j= i+1;j<a.length;j++) {
-				if(a[i]>a[j]) {
-					temp=a[i];
-					a[i]=a[j];
-					a[j]=temp;
+		for(int i=0;i<p.length-1;i++) {
+			for(int j= i+1;j<p.length;j++) {
+				if(p[i]>p[j]) {
+					temp=p[i];
+					p[i]=p[j];
+					p[j]=temp;
 				}
 			}
 		}
-		for(int i=0;i<a.length;i++) {
-			System.out.println(a[i]);
+		for(int i=0;i<p.length;i++) {
+			System.out.println(p[i]);
 		}
 		
 
---
+++ src/sorting/NewSort.java
@@ -0,0 +1,42 @@
+package sorting;
+
+import java.util.Scanner;
+
+public class NewSort {
+	void sort(int arr[]) 
+    { 
+        int n = arr.length; 
+  
+        // One by one move boundary of unsorted subarray 
+        for (int i = 0; i < n-1; i++) 
+        { 
+            // Find the minimum element in unsorted array 
+            int min_idx = i; 
+            for (int j = i+1; j < n; j++) 
+                if (arr[j] < arr[min_idx]) 
+                    min_idx = j; 
+  
+            // Swap the found minimum element with the first 
+            // element 
+            int temp = arr[min_idx]; 
+            arr[min_idx] = arr[i]; 
+            arr[i] = temp; 
+        } 
+    }
+
+	public static void main(String[] args) {
+		int[] p = new int[5];
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the list of numbers");
+		for(int i=0;i<p.length;i++) {
+			p[i]= in.nextInt();
+		}
+		NewSort n = new NewSort();
+		n.sort(p);
+		for(int i=0;i<p.length;i++) {
+			System.out.println(p[i]);
+		}
+        
+	}
+
+}
